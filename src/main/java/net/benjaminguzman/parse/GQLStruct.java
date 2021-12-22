da569fd1c99ddc22acd4bd661d0f9fece012f3dc
Benjamín Guzmán <bg@benjaminguzman.dev>
2021-12-22 13:12:08 -0600

    Added copyright header to source code

--- src/main/java/net/benjaminguzman/parse/GQLStruct.java
+++ src/main/java/net/benjaminguzman/parse/GQLStruct.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021. Benjamín Antonio Velasco Guzmán
+ * Author: Benjamín Antonio Velasco Guzmán <bg@benjaminguzman.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.benjaminguzman.parse;
 
 import org.jetbrains.annotations.NotNull;
@@ -43,7 +61,7 @@ public abstract class GQLStruct extends GQLDataType {
 	 * <p>
 	 * Comments are delimited by """
 	 *
-	 * @param str string from which fields will be parsed
+	 * @param str    string from which fields will be parsed
 	 * @param struct reference to the struct containing the fields that will be parsed
 	 * @return a list of parsed fields
 	 * @throws InvalidGQLSyntax if some field couldn't be parsed because it is invalid
